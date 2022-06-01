c95cc754f9c828af0b6e90ef8b204bc6f9e8d589
Christian <christiannmenezes@gmail.com>
2022-06-01 20:47:11 -0300

    Handling page redirect and flash messages

--- src/libs/helpers.php
+++ src/libs/helpers.php
@@ -32,3 +32,73 @@ function is_get_request(): bool
     return strtoupper($_SERVER['REQUEST_METHOD']) === 'GET';
 }
 
+/**
+ * @param array $errors
+ * @param string $field
+ * @return string
+ */
+function error_class(array $errors, string $field): string
+{
+    return isset($errors[$field]) ? 'error' : '';
+}
+
+/**
+ * Redirect to another URL
+ *
+ * @param string $url
+ * @return void
+ */
+function redirect_to(string $url): void
+{
+    header('Location:' . $url);
+    exit;
+}
+
+/**
+ * Redirect to a URL with data stored in the items array
+ *
+ * @param string $url
+ * @param array $items
+ * @return void
+ */
+function redirect_with(string $url, array $items): void
+{
+    foreach ($items as $key => $value) {
+        $_SESSION[$key] = $value;
+    }
+
+    redirect_to($url);
+}
+
+/**
+ * Redirect to a URL with a flash message
+ *
+ * @param string $url
+ * @param string $message
+ * @param string $type
+ * @return void
+ */
+function redirect_with_message(string $url, string $message, string $type=FLASH_SUCCESS)
+{
+    flash('flash_' . uniqid(), $message, $type);
+    redirect_to($url);
+}
+
+/**
+ * Flash data specified by $keys from the $_SESSION
+ * @param ...$keys
+ * @return array
+ */
+function session_flash(...$keys): array
+{
+    $data = [];
+    foreach ($keys as $key) {
+        if (isset($_SESSION[$key])) {
+            $data[] = $_SESSION[$key];
+            unset($_SESSION[$key]);
+        } else {
+            $data[] = [];
+        }
+    }
+    return $data;
+}
