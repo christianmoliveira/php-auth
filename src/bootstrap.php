b01134552e0ddae4e5c7198a22522ac118e60139
Christian <christiannmenezes@gmail.com>
2022-06-01 20:37:37 -0300

    Added sanitization, validation and filter functions

---
+++ src/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once __DIR__ . '/libs/helpers.php';
+require_once __DIR__ . '/libs/sanitization.php';
+require_once __DIR__ . '/libs/validation.php';
+require_once __DIR__ . '/libs/filter.php';
