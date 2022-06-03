00cfa6216d64e5e19fea7aad892ff9a88e689fb5
Christian <christiannmenezes@gmail.com>
2022-06-02 22:30:31 -0300

    Added functions to handle authenticated user

--- src/auth.php
+++ src/auth.php
@@ -23,3 +23,86 @@ function register_user(string $email, string $username, string $password, bool $
 
     return $statement->execute();
 }
+
+/**
+ * @param string $username
+ * @param string $password
+ * @return bool
+ */
+function login(string $username, string $password): bool
+{
+    $user = find_user_by_username($username);
+
+    // if user found, check the password
+    if ($user && password_verify($password, $user['password'])) {
+
+        // prevent session fixation attack
+        session_regenerate_id();
+
+        // set username in the session
+        $_SESSION['username'] = $user['username'];
+        $_SESSION['user_id']  = $user['id'];
+
+        return true;
+    }
+
+    return false;
+}
+
+/**
+ * @return void
+ */
+function logout(): void
+{
+    if (is_user_logged_in()) {
+        unset($_SESSION['username'], $_SESSION['user_id']);
+        session_destroy();
+        redirect_to('login.php');
+    }
+}
+
+/**
+ * @return mixed|null
+ */
+function current_user()
+{
+    if (is_user_logged_in()) {
+        return $_SESSION['username'];
+    }
+    return null;
+}
+
+/**
+ * @return bool
+ */
+function is_user_logged_in(): bool
+{
+    return isset($_SESSION['username']);
+}
+
+/**
+ * @return void
+ */
+function require_login(): void
+{
+    if (!is_user_logged_in()) {
+        redirect_to('login.php');
+    }
+}
+
+/**
+ * @param string $username
+ * @return mixed
+ */
+function find_user_by_username(string $username)
+{
+    $sql = 'SELECT username, password
+            FROM users
+            WHERE username=:username';
+
+    $statement = db()->prepare($sql);
+    $statement->bindValue(':username', $username, PDO::PARAM_STR);
+    $statement->execute();
+
+    return $statement->fetch(PDO::FETCH_ASSOC);
+}
